c2151d7030847cb4d1869c108860de1e17255267
Matthew Bonanni <mbonanni001@gmail.com>
2023-12-24 21:14:33 -0500

    Rename

---
+++ src/numerics/face_reconstruction.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file face_reconstruction.cpp
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief Face reconstruction class implementation.
+ * @version 0.1
+ * @date 2023-12-24
+ * 
+ * @copyright Copyright (c) 2023 Matthew Bonanni
+ * 
+ */
+
+#include "face_reconstruction.h"
+
+FaceReconstruction::FaceReconstruction() {
+    // Empty
+}
+
+FaceReconstruction::~FaceReconstruction() {
+    // Empty
+}
---
+++ src/numerics/face_reconstruction.h
@@ -0,0 +1,30 @@
+/**
+ * @file face_reconstruction.h
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief Face reconstruction class declaration.
+ * @version 0.1
+ * @date 2023-12-24
+ * 
+ * @copyright Copyright (c) 2023 Matthew Bonanni
+ * 
+ */
+
+#ifndef FACE_RECONSTRUCTION_H
+#define FACE_RECONSTRUCTION_H
+
+class FaceReconstruction {
+    public:
+        /**
+         * @brief Construct a new Face Reconstruction object
+         */
+        FaceReconstruction();
+
+        /**
+         * @brief Destroy the Face Reconstruction object
+         */
+        ~FaceReconstruction();
+    protected:
+    private:
+};
+
+#endif // FACE_RECONSTRUCTION_H
