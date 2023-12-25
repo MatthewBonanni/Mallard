fd52a628c9f949bfa13cc32268829d6146662381
Matthew Bonanni <mbonanni001@gmail.com>
2023-12-24 20:50:05 -0500

    Started implementing flux reconstruction

---
+++ src/numerics/flux_reconstruction.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file flux_reconstruction.cpp
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief Flux reconstruction class implementation.
+ * @version 0.1
+ * @date 2023-12-24
+ * 
+ * @copyright Copyright (c) 2023 Matthew Bonanni
+ * 
+ */
+
+#include "flux_reconstruction.h"
+
+FluxReconstruction::FluxReconstruction() {
+    // Empty
+}
+
+FluxReconstruction::~FluxReconstruction() {
+    // Empty
+}
---
+++ src/numerics/flux_reconstruction.h
@@ -0,0 +1,30 @@
+/**
+ * @file flux_reconstruction.h
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief Flux reconstruction class declaration.
+ * @version 0.1
+ * @date 2023-12-24
+ * 
+ * @copyright Copyright (c) 2023 Matthew Bonanni
+ * 
+ */
+
+#ifndef FLUX_RECONSTRUCTION_H
+#define FLUX_RECONSTRUCTION_H
+
+class FluxReconstruction {
+    public:
+        /**
+         * @brief Construct a new Flux Reconstruction object
+         */
+        FluxReconstruction();
+
+        /**
+         * @brief Destroy the Flux Reconstruction object
+         */
+        ~FluxReconstruction();
+    protected:
+    private:
+};
+
+#endif // FLUX_RECONSTRUCTION_H
