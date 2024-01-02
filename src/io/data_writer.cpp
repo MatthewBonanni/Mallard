bbb59d07016ce130f811b09da21cd1a81c8ac690
Matthew Bonanni <mbonanni001@gmail.com>
2024-01-01 22:34:18 -0500

    Start implementing IO stuff

---
+++ src/io/data_writer.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file data_writer.cpp
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief DataWriter class implementation.
+ * @version 0.1
+ * @date 2024-01-01
+ * 
+ * @copyright Copyright (c) 2024 Matthew Bonanni
+ * 
+ */
+
+#include "data_writer.h"
+
+DataWriter::DataWriter() {
+    // Empty
+}
+
+DataWriter::~DataWriter() {
+    // Empty
+}
---
+++ src/io/data_writer.h
@@ -0,0 +1,30 @@
+/**
+ * @file data_writer.h
+ * @author Matthew Bonanni (mbonanni001@gmail.com)
+ * @brief DatWriter class declaration.
+ * @version 0.1
+ * @date 2024-01-01
+ * 
+ * @copyright Copyright (c) 2024 Matthew Bonanni
+ * 
+ */
+
+#ifndef DATA_WRITER_H
+#define DATA_WRITER_H
+
+class DataWriter {
+    public:
+        /**
+         * @brief Construct a new DataWriter object
+         */
+        DataWriter();
+
+        /**
+         * @brief Destroy the DataWriter object
+         */
+        ~DataWriter();
+    protected:
+    private:
+};
+
+#endif // DATA_WRITER_H
